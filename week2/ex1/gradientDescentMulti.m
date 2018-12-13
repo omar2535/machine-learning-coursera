c09e9abf3978734a24c54d8e2661f4a441866d33
omar2535 <omar2535@gmail.com>
2018-12-12 19:55:56 -0800

    completed ex1 with grad descent multi and the normal equation

--- week2/ex1/gradientDescentMulti.m
+++ week2/ex1/gradientDescentMulti.m
@@ -16,9 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
+    theta = theta - alpha * (1/m) * (X' * ((X * theta) - y))
 
-
-
+    
 
 
 
