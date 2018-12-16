f00ea49df251992da2cd7925f99e8a53588fb129
Omar <omar2535@gmail.com>
2018-12-16 11:08:06 -0800

    added gradient descent

--- python/linear_regression/single_variable_linear_regression.py
+++ python/linear_regression/single_variable_linear_regression.py
@@ -1,8 +1,28 @@
 import numpy as np
 import csv
 
-# load in the data
-data = np.loadtxt(open("ex1data1.csv", "rb"), delimiter=",", skiprows=1)
+def main():
+    # load in the data
+    data = np.loadtxt(open("ex1data1.csv", "rb"), delimiter=",", skiprows=1)
 
-X = data[:,0]
-y = data[:,1]
+    X = data[:,0]
+    y = data[:,1]
+    m = y.size
+    theta = gradient_descent(X,y,0,0.01,1500)
+        
+
+
+# computes the cost
+def cost_function(X, theta, m):
+    h = theta * X
+    h_y = (h - y) ** 2
+    cost = 1/(2*m) * sum(h_y)
+    return cost
+
+def gradient_descent(X, y, theta, alpha, iterations):
+    m = y.size
+    for _ in range(iterations):
+        theta = theta - alpha * (1/m) * (X.T * ((X * theta) - y))
+    return theta
+
+main()
