dd271114252b3e87d17868d5cfdefda4fd304569
Omar <omar2535@gmail.com>
2018-12-16 19:28:47 -0800

    completed gradient descent

--- python/linear_regression/single_variable_linear_regression.py
+++ python/linear_regression/single_variable_linear_regression.py
@@ -1,5 +1,6 @@
 import numpy as np
 import csv
+import matplotlib.pyplot as plt
 
 def main():
     # load in the data
@@ -8,21 +9,32 @@ def main():
     X = data[:,0]
     y = data[:,1]
     m = y.size
-    theta = gradient_descent(X,y,0,0.01,1500)
-        
+    theta = gradient_descent(X, y, m, np.array([0]), 0.01, 1500)
+    print("theta is", theta)
 
 
 # computes the cost
-def cost_function(X, theta, m):
+def cost_function(X, y, theta, m):
     h = theta * X
     h_y = (h - y) ** 2
     cost = 1/(2*m) * sum(h_y)
     return cost
 
-def gradient_descent(X, y, theta, alpha, iterations):
-    m = y.size
-    for _ in range(iterations):
-        theta = theta - alpha * (1/m) * (X.T * ((X * theta) - y))
+def gradient_descent(X, y, m, theta, alpha, iterations):
+    for i in range(iterations):
+        print("theta", theta)
+        print("cost:", cost_function(X,y,theta,m))
+        theta = theta - alpha * (1/m) * np.matmul(X.T, ((X * theta) - y))
+        
+        #for plotting
+        # plt.plot(X, theta*X)
+        # plt.scatter(X, y)
+        # plt.xlabel('Population', fontsize=18)
+        # plt.ylabel('Profits', fontsize=16)
+        # plt.pause(0.05)
+        # if i != iterations-1:
+        #     plt.gcf().clear()
+    #plt.show()
     return theta
 
 main()
